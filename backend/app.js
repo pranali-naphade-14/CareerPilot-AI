99de7378874d1a961ef6fcf2fd302e568a68b874
Pranali Naphade <pranalinaphade15.com>
2026-09-25 22:48:27 +0530

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
-const express=require("express")
-const cookieParser=require("cookie-parser")
-const authRouter=require("./routes/auth.routes")
-const interviewRouter=require("./routes/interview.routes")
-const cors=require("cors")
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const authRouter = require("./routes/auth.routes");
+const interviewRouter = require("./routes/interview.routes");
+const cors = require("cors");
 
-const app=express();
+const app = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-app.use("/auth",authRouter)
-app.use("/interview",interviewRouter)
+    origin: "https://careerpilot-ai-1-7s2u.onrender.com",
+    credentials: true
+}));
 
-module.exports=app
+app.use("/auth", authRouter);
+app.use("/interview", interviewRouter);
+
+module.exports = app;
